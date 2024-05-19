2f262cc79c205d67eb3f711aeeeae24d130bec7f
bestVickey <94274994+bestVickey@users.noreply.github.com>
2024-05-19 19:04:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '伟奇',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oXYV36jhIzuh1jPawzcZ9xLvVQ88',
+      id: '2xujOQpOsnlbchMfqnPfL5xsyfc_CAXFQyswCbB2tqQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'jbJNiIHETKYHWv61yIjap7RhiEQ_aTjlI2KrfAcHAQ8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
