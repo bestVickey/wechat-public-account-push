107e10dd7c61564f3388a197a88862fefdda5bd3
bestVickey <94274994+bestVickey@users.noreply.github.com>
2024-07-12 19:36:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -26,7 +26,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oXYV36s3FxyUs2ppmCvi2w8fzD_I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'yOtSUtiLAXzURpAC3I8J5zqAYhQMio__M74gcfAyqOg',
+      useTemplateId: 'nIclgLOi52OJyw3wOlTkIkjU0OrB0GOQbDOjKIOStvI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-01',
       festivals: [
